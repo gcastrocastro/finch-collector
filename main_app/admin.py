c5e1bbdfc5f85d73101ca2ec0c11a6aad4e29081
gcastrocastro <gcastrocastro.25@gmail.com>
2023-06-02 19:19:05 -0400

    wip: register Sponsor in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Finch, Feeding
+from .models import Finch, Feeding, Sponsor
 
 admin.site.register(Finch)
 admin.site.register(Feeding)
+admin.site.register(Sponsor)
